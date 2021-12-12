45006a2942a1b9f07743816e880de9ff30d66de0
Stoyan Grozdanov <st.grozdanov@abv.bg>
2021-12-12 21:05:16 +0200

    Register page created

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import { viewAllPage } from './views/allRecordsView.js';
 import page from '../node_modules/page/page.mjs';
 import { setUp } from './middlewares/setUpMidware.js';
 import { loginPage } from './views/loginView.js';
+import { registerPage } from './views/registerView.js';
 
 page('/', setUp, viewAllPage)
 page('/login', setUp, loginPage);
-// page('/register', setUp, registerPage);
+page('/register', setUp, registerPage);
 // page('/add', setUp, createPage);
 // page('/details/:id', setUp, detailsPage);
 // page('/edit/:id', setUp, editPage);
--- src/views/registerView.js
+++ src/views/registerView.js
@@ -2,31 +2,38 @@ import { html } from '../../node_modules/lit-html/lit-html.js';
 import { register } from '../io/registry.js'
 
 const registerTemplate = (context) => html`
-<div class="row space-top">
-    <div class="col-md-12">
-        <h1>Register New User</h1>
-        <p>Please fill all fields.</p>
-    </div>
-</div>
-<form id="register-form">
-    <div class="row space-top">
-        <div class="col-md-4">
-            <div class="form-group">
-                <label class="form-control-label" for="email">Email</label>
-                <input class="form-control" id="email" type="text" name="email">
-            </div>
-            <div class="form-group">
-                <label class="form-control-label" for="password">Password</label>
-                <input class="form-control" id="password" type="password" name="password">
-            </div>
-            <div class="form-group">
-                <label class="form-control-label" for="rePass">Repeat</label>
-                <input class="form-control" id="rePass" type="password" name="rePass">
-            </div>
-            <input @click=${(e) => registerHandler(e, context)} type="submit" class="btn btn-primary" value="Register" />
-        </div>
-    </div>
-</form>
+<section id="register-page" class="register formData">
+    <form id="register-form" action="" method="">
+        <fieldset>
+            <legend>Регистрация</legend>
+            <p class="field">
+                <label for="email">Имейл</label>
+                <span class="input">
+                    <input type="text" name="email" id="email" placeholder="Email">
+                </span>
+            </p>
+            <p class="field">
+                <label for="username">Потребителско име</label>
+                <span class="input">
+                    <input type="text" name="username" id="username" placeholder="Username">
+                </span>
+            </p>
+            <p class="field">
+                <label for="password">Парола</label>
+                <span class="input">
+                    <input type="password" name="password" id="password" placeholder="Password">
+                </span>
+            </p>
+            <p class="field">
+                <label for="repeat-pass">Повтори паролата</label>
+                <span class="input">
+                    <input type="password" name="confirm-pass" id="repeat-pass" placeholder="Repeat Password">
+                </span>
+            </p>
+            <input @click=${(e) => registerHandler(e, context)} class="button submit" type="submit" value="Регистрирай">
+        </fieldset>
+    </form>
+</section>
 `;
 
 export function registerPage(context) {
@@ -37,15 +44,18 @@ async function registerHandler(e, context) {
     e.preventDefault();
 
     const form = new FormData(document.getElementById('register-form'));
+    const username = form.get('username');
     const email = form.get('email');
     const password = form.get('password');
-    const rePassword = form.get('rePass');
+    const rePassword = form.get('confirm-pass');
 
-    if (email == '' || password == '' || rePassword == '') {
+    if (email == '' || password == '' || rePassword == '' || username == '') {
         return alert('All fields are required!');
     } else if (password !== rePassword) {
-        return alert('Passwords don\'nt match!')
+        return alert('Passwords don\'t match!')
     }
-    await register(email, password);
+    await register(username, email, password);
     context.page.redirect('/');
+    const navigationHomeButton = document.querySelector('#catalogLink');
+    navigationHomeButton.classList.add('active');
 }
