81404a8f4f8c4d3f6b3a253b3f8ddbfeb1234fe5
Stoyan Grozdanov <st.grozdanov@abv.bg>
2022-05-12 13:18:59 +0300

    Code refactoring

--- src/utils/backToTopButton.js
+++ src/utils/backToTopButton.js
@@ -1,3 +1,6 @@
+const SCROLL_TO_SCREEN_TOP_VALUE = 0;
+const SCROLL_TO_RECIPE_VIEWPORT_VALUE = 400;
+
 export function buttonToTop() {
     const backToTopButton = document.querySelector(".button-to-top");
 
@@ -5,9 +8,9 @@ export function buttonToTop() {
         let topPosition;
 
         if (window.matchMedia('(max-device-width: 700px)').matches) {
-            topPosition = 0;
+            topPosition = SCROLL_TO_SCREEN_TOP_VALUE;
         } else {
-            topPosition = 400;
+            topPosition = SCROLL_TO_RECIPE_VIEWPORT_VALUE;
         }
 
         window.scrollTo({
