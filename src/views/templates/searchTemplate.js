d3db12743c3d60621cb72cb17bd5941e961ecd51
Stoyan Grozdanov <st.grozdanov@abv.bg>
2022-05-20 17:11:23 +0300

    Search field autocomplete is turned of

--- src/views/templates/searchTemplate.js
+++ src/views/templates/searchTemplate.js
@@ -5,7 +5,7 @@ export const searchTemplate = (ctx) => html`
     <form @submit=${(e) => search(e, ctx)}>
         <img @click=${(e) => search(e, ctx)} id="find-img"
         src="../../static/images/istockphoto-1068237878-170667a-removebg-preview.png"> 
-        <input type="text" id="myInput" placeholder="Търсете по име на рецептата...">
+        <input type="search" id="myInput" placeholder="Търсете по име на рецептата..." autocomplete="off">
     </form>
 `;
 
