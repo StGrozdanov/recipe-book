de1ec7e30ba46fcbc4d4a9eabbe67260141e103b
StGrozdanov <74171353+StGrozdanov@users.noreply.github.com>
2022-06-20 23:38:49 +0300

    Implemented status bar and nav bar dynamic design depending on color theme on android devices

--- mobile/App.js
+++ mobile/App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ThemeProvider } from './contexts/ThemeContext';
 import Login from './components/Login/Login';
 import Dashboard from './components/Dashboard/Dashboard';
 import Panel from './components/ApplicationPanel/Panel';
@@ -7,37 +8,37 @@ import Users from './components/Users/Users';
 import Recipes from './components/Recipes/Recipes';
 import Comments from './components/Comments/Comments';
 import Settings from './components/Settings/Settings';
-import { ThemeProvider } from './contexts/ThemeContext';
+import { StatusBar, StatusBarStyle } from 'expo-status-bar';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <ThemeProvider>
+    <>
+      <ThemeProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false, animation: 'none' }}>
 
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false, animation: 'none' }}>
+            <Stack.Screen name='Home' component={Login} />
+            <Stack.Screen name="Dashboard">
+              {(props) => <Panel {...props} content={<Dashboard />} />}
+            </Stack.Screen>
+            <Stack.Screen name="Users">
+              {(props) => <Panel {...props} content={<Users />} />}
+            </Stack.Screen>
+            <Stack.Screen name="Recipes">
+              {(props) => <Panel {...props} content={<Recipes />} />}
+            </Stack.Screen>
+            <Stack.Screen name="Comments">
+              {(props) => <Panel {...props} content={<Comments />} />}
+            </Stack.Screen>
+            <Stack.Screen name="Settings">
+              {(props) => <Panel {...props} content={<Settings />} />}
+            </Stack.Screen>
 
-          <Stack.Screen name='Home' component={Login} />
-          <Stack.Screen name="Dashboard">
-            {(props) => <Panel {...props} content={<Dashboard />} />}
-          </Stack.Screen>
-          <Stack.Screen name="Users">
-            {(props) => <Panel {...props} content={<Users />} />}
-          </Stack.Screen>
-          <Stack.Screen name="Recipes">
-            {(props) => <Panel {...props} content={<Recipes />} />}
-          </Stack.Screen>
-          <Stack.Screen name="Comments">
-            {(props) => <Panel {...props} content={<Comments />} />}
-          </Stack.Screen>
-          <Stack.Screen name="Settings">
-            {(props) => <Panel {...props} content={<Settings />} />}
-          </Stack.Screen>
-
-        </Stack.Navigator>
-      </NavigationContainer>
-
-    </ThemeProvider>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
+    </>
   );
 }
--- mobile/components/ApplicationPanel/Panel.js
+++ mobile/components/ApplicationPanel/Panel.js
@@ -1,17 +1,31 @@
 import { View } from "react-native";
 import { adminPanel } from "./PanelStyleSheet";
+import { useThemeContext } from "../../contexts/ThemeContext";
 import Navigation from "../Navigation/Navigation";
 import Header from "../Header/Header";
-import { useThemeContext } from "../../contexts/ThemeContext";
+import { StatusBar } from "expo-status-bar";
+import * as NavigationBar from 'expo-navigation-bar';
 
 export default function Panel({ navigation, content }) {
     const { theme } = useThemeContext();
 
+    const androidNavAndStatusBarColors = theme == 'light' ? 'dark' : 'light';
+
+    const androidNavBarBackground = theme == 'light' ? '#EFEEFE' : 'black';
+    NavigationBar.setBackgroundColorAsync(androidNavBarBackground);
+    NavigationBar.setButtonStyleAsync(androidNavAndStatusBarColors);
+
     return (
-        <View style={adminPanel[theme + 'Container']}>
-            <Header />
-            {content}
-            <Navigation />
-        </View>
+        <>
+            <StatusBar
+                backgroundColor={theme == 'light' ? "#EFEEFE" : "rgba(124,113,192,0.9)"}
+                style={androidNavAndStatusBarColors}
+            />
+            <View style={adminPanel[theme + 'Container']}>
+                <Header />
+                {content}
+                <Navigation />
+            </View>
+        </>
     );
 }
