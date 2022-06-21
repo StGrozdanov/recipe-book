f85c949fab1a8ae4259c42d537f9793820164084
StGrozdanov <74171353+StGrozdanov@users.noreply.github.com>
2022-06-21 11:48:35 +0300

    Fixed component child missing keyprops and improved valid cell detection technique in the table body

--- mobile/components/Table/TableBody/TableBody.js
+++ mobile/components/Table/TableBody/TableBody.js
@@ -7,26 +7,26 @@ import Location from "./TableBodyTypes/TableBodyLocation";
 import { useThemeContext } from "../../../contexts/ThemeContext";
 
 const CELL_TYPES = {
-    Status: (cellData) => <Status status={cellData} />,
-    Owner: (location) => <Owner pointer={location} />,
-    Location: (location) => <Location pointer={location} />,
+    Status: (cellData, cellKey) => <Status status={cellData} key={cellKey} />,
+    Owner: (location, cellKey) => <Owner pointer={location} key={cellKey} />,
+    Location: (location, cellKey) => <Location pointer={location} key={cellKey} />,
 }
 
 export default function TableBody({ isToggled, data }) {
-    data = Object.entries(data).slice(2);
-    data.pop();
+    data = Object.entries(data);
+
     const { theme } = useThemeContext();
 
     return (
         <View style={[tableBodyStyles[theme + 'AdditionalData'], !isToggled && tableBodyStyles.toggledData]}>
             {
-                data.map(content => {
+                data.map((content, id) => {
                     const cellHeading = content[0];
                     const cellData = content[1];
 
                     return CELL_TYPES.hasOwnProperty(cellHeading)
-                        ? CELL_TYPES[cellHeading](cellData)
-                        : <Cell heading={cellHeading} data={cellData} />
+                        ? CELL_TYPES[cellHeading](cellData, id)
+                        : <Cell heading={cellHeading} data={cellData} key={id} />
                 })
             }
         </View>
