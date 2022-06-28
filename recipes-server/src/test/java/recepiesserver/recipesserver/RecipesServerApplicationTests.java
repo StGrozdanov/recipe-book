946fb80e1a195d961e2b91f930177dbb8cd28217
StGrozdanov <74171353+StGrozdanov@users.noreply.github.com>
2022-06-28 16:02:58 +0300

    Innitial custom REST server commit

---
+++ recipes-server/src/test/java/recepiesserver/recipesserver/RecipesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package recepiesserver.recipesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
