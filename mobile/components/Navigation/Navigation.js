7fb36a4ed4c1ebc772635789856b599acde4f64e
StGrozdanov <74171353+StGrozdanov@users.noreply.github.com>
2022-07-21 11:53:03 +0300

    Working go to website icon

--- mobile/components/Navigation/Navigation.js
+++ mobile/components/Navigation/Navigation.js
@@ -8,37 +8,44 @@ import { faCommentDots } from '@fortawesome/free-solid-svg-icons/faCommentDots';
 import { faGear } from '@fortawesome/free-solid-svg-icons/faGear';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { useThemeContext } from "../../hooks/useThemeContext";
+import { useOuterURL } from '../../hooks/useOuterURL';
 
 export default function Navigation() {
     const navigationRoute = useRoute();
     const navigator = useNavigation();
     const { theme } = useThemeContext();
+    
+    async function redirectHandler() {
+        await useOuterURL(`https://recepti-na-shushanite.web.app`);
+    }
 
     const currentPageName = navigationRoute.name;
 
     return (
         <View style={adminPanelNav[theme + 'Navigation']}>
+            <TouchableOpacity onPress={redirectHandler}>
                 <Image style={adminPanelNav[theme + 'WebsiteLogo']} source={require('../../assets/cooking.png')} />
-                <TouchableOpacity onPress={() => navigator.navigate('Dashboard')}>
-                    <FontAwesomeIcon style={adminPanelNav[theme + 'NavItem']} size={26} icon={faChartSimple} />
-                    <ImageBackground style={currentPageName == 'Dashboard' && adminPanelNav.selected} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigator.navigate('Users')}>
-                    <FontAwesomeIcon style={adminPanelNav[theme + 'NavItem']} size={26} icon={faUserLarge} />
-                    <ImageBackground style={currentPageName == 'Users' && adminPanelNav.selected} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigator.navigate('Recipes')}>
-                    <FontAwesomeIcon style={adminPanelNav[theme + 'NavItem']} size={26} icon={faBowlRice} />
-                    <ImageBackground style={currentPageName == 'Recipes' && adminPanelNav.selected} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigator.navigate('Comments')}>
-                    <FontAwesomeIcon style={adminPanelNav[theme + 'NavItem']} size={26} icon={faCommentDots} />
-                    <ImageBackground style={currentPageName == 'Comments' && adminPanelNav.selected} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigator.navigate('Settings')}>
-                    <FontAwesomeIcon style={adminPanelNav[theme + 'NavItem']} size={26} icon={faGear} />
-                    <ImageBackground style={currentPageName == 'Settings' && adminPanelNav.selected} />
-                </TouchableOpacity>            
-            </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigator.navigate('Dashboard')}>
+                <FontAwesomeIcon style={adminPanelNav[theme + 'NavItem']} size={26} icon={faChartSimple} />
+                <ImageBackground style={currentPageName == 'Dashboard' && adminPanelNav.selected} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigator.navigate('Users')}>
+                <FontAwesomeIcon style={adminPanelNav[theme + 'NavItem']} size={26} icon={faUserLarge} />
+                <ImageBackground style={currentPageName == 'Users' && adminPanelNav.selected} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigator.navigate('Recipes')}>
+                <FontAwesomeIcon style={adminPanelNav[theme + 'NavItem']} size={26} icon={faBowlRice} />
+                <ImageBackground style={currentPageName == 'Recipes' && adminPanelNav.selected} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigator.navigate('Comments')}>
+                <FontAwesomeIcon style={adminPanelNav[theme + 'NavItem']} size={26} icon={faCommentDots} />
+                <ImageBackground style={currentPageName == 'Comments' && adminPanelNav.selected} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigator.navigate('Settings')}>
+                <FontAwesomeIcon style={adminPanelNav[theme + 'NavItem']} size={26} icon={faGear} />
+                <ImageBackground style={currentPageName == 'Settings' && adminPanelNav.selected} />
+            </TouchableOpacity>
+        </View>
     );
 }
