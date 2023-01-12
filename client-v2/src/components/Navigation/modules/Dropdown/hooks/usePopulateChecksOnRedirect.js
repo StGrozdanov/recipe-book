63440705d5b0529d569001670491a2dbd6b9eaf0
StGrozdanov <74171353+StGrozdanov@users.noreply.github.com>
2023-01-12 23:17:06 +0200

    fixed dropdown bug

--- client-v2/src/components/Navigation/modules/Dropdown/hooks/usePopulateChecksOnRedirect.js
+++ client-v2/src/components/Navigation/modules/Dropdown/hooks/usePopulateChecksOnRedirect.js
@@ -3,28 +3,40 @@ import { useLocation } from "react-router-dom";
 
 export function usePopulateChecksOnRedirect(setChecksHandler) {
     const location = useLocation();
-    useEffect(() => expandCheckboxWithSelectedCategories(), [location.pathname]);
 
-    function expandCheckboxWithSelectedCategories() {
+    useEffect(() => {
+        if (location.pathname == '/categories' && location.search.length > 0) {
+            populateCheckboxes();
+        } else {
+            cleanupPopulatedCheckboxes();
+        }
+    }, [location.pathname]);
+
+
+
+    function populateCheckboxes() {
+        const checks = document.querySelectorAll('[type=checkbox]');
+        const params = decodeURI(location.search.split('=')[1].split('&') || '').split(',');
+
+        Array.from(checks)
+            .filter(c => params.includes(c.defaultValue))
+            .map(element => element.checked = true);
+
+        document.getElementById('all-categories').checked = false;
+
+        setChecksHandler(params);
+    }
+
+    function cleanupPopulatedCheckboxes() {
         const checks = document.querySelectorAll('[type=checkbox]');
 
-        if (location.search) {
-            const params = decodeURI(location.search.split('=')[1].split('&') || '').split(',');
-
-            Array.from(checks)
-                .filter(c => params.includes(c.defaultValue))
-                .map(element => element.checked = true);
-
-            document.getElementById('all-categories').checked = false;
-
-            setChecksHandler(params);
-        } else {
+        if (document.getElementById('all-categories').checked == false) {
             Array.from(checks)
                 .filter(c => c.checked = true)
                 .map(element => element.checked = false);
 
             document.getElementById('all-categories').checked = true;
-            
+
             setChecksHandler([]);
         }
     }
