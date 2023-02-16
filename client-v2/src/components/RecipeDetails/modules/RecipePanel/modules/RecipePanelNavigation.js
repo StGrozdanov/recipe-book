76322787697bdadabf2fabbf8226a25a98fef4d2
StGrozdanov <74171353+StGrozdanov@users.noreply.github.com>
2023-02-16 19:13:15 +0200

    selected nav styling

--- client-v2/src/components/RecipeDetails/modules/RecipePanel/modules/RecipePanelNavigation.js
+++ client-v2/src/components/RecipeDetails/modules/RecipePanel/modules/RecipePanelNavigation.js
@@ -2,22 +2,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping, faComment, faPenToSquare, faShareNodes, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import styles from './RecipePanelNavigation.module.scss';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+const selectedStyle = { backgroundSize: '100% 0.15em', color: '#57595fc9' };
 
 export default function RecipePanelNavigation({ recipeId }) {
+    const [selected, setSelected] = useState('products');
     const navigate = useNavigate();
+
+    function redirectOnSelectHandler(URL, buttonName) {
+        setSelected(buttonName);
+        navigate(URL);
+    }
 
     return (
         <nav className={styles.navigation}>
             <ul>
                 <FontAwesomeIcon
+                    style={selected == 'products' && selectedStyle}
                     icon={faCartShopping}
                     className={styles['nav-icon']}
-                    onClick={() => navigate(`/details/${recipeId}`)}
+                    onClick={() => redirectOnSelectHandler(`/details/${recipeId}`, 'products')}
                 />
                 <FontAwesomeIcon
+                    style={selected == 'comments' && selectedStyle}
                     icon={faComment}
                     className={styles['nav-icon']}
-                    onClick={() => navigate(`/details/${recipeId}/comments`)}
+                    onClick={() => redirectOnSelectHandler(`/details/${recipeId}/comments`, 'comments')}
                 />
                 <FontAwesomeIcon
                     icon={faPenToSquare}
